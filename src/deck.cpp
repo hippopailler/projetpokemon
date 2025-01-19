0f33ba3a60d58782b480a51a08c7c73f08ce28a9
Ollia0 <dodo.diallo@gmail.com>
2025-01-19 16:22:31 +0100

    feat: added energy support to decks

--- src/deck.cpp
+++ src/deck.cpp
@@ -3,8 +3,8 @@
 #include <random> 
 #include <chrono> 
 
-Deck::Deck(std::vector<Card> cards):
-    _cards(cards) {};
+Deck::Deck(std::vector<Card> cards, std::set<typeEnergy> energyTypes) :
+    _cards(cards),  _energyTypes(energyTypes) {};
 
 void Deck::shuffle(){
     // Ici c'est possible de remplacer seed par un entier fixe pour fixer la RNG (tests)
@@ -17,4 +17,20 @@ Card& Deck::draw(){
     Card& card = _cards.back();
     _cards.pop_back();
     return card;
+}
+
+std::vector<Card> Deck::draw(int n){
+    std::vector<Card> cards;
+    for (int i = 0; i < n; i++){
+        cards.push_back(draw());
+    }
+    return cards;
+}
+
+typeEnergy Deck::randomEnergy() const{
+    std::vector<typeEnergy> energyTypes(_energyTypes.begin(), _energyTypes.end());
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine e(seed);
+    std::uniform_int_distribution<int> distribution(0, energyTypes.size() - 1);
+    return energyTypes[distribution(e)];
 }
--- src/deck.h
+++ src/deck.h
@@ -2,19 +2,24 @@
 #define DECK
 
 #include "card.h"
+#include "energy.h"
 #include <vector>
+#include <set>
 
 class Deck{
 public:
-    Deck(std::vector<Card> cards);
+    Deck(std::vector<Card> cards, std::set<typeEnergy> energyTypes);
 // Mutateurs
     void shuffle();
     Card& draw();
-    void draw(int);
+    std::vector<Card> draw(int);
 // Accesseurs
     std::vector<Card> cards() const;
+    typeEnergy randomEnergy() const;
+
 private:
     std::vector<Card> _cards;
+    std::set<typeEnergy> _energyTypes;
 };
 
 #endif
